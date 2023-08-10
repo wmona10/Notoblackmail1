e3c995a3c48efe5d8655140cfb80bc3848801d10
wmona10 <141957176+wmona10@users.noreply.github.com>
2023-08-10 22:00:47 +0300

    Code

---
+++ project.py
@@ -0,0 +1,18 @@
+import webbrowser
+print("Hello")
+print("Note: This tool is designed only for valid use, any other use that is not related to the purpose of the tool")
+print("____________________")
+print("[1.اداة التتبع 2.حذف الصور المفبركة 3.موقع الابلاغ 4.سحب الصور 5.سحب الصور² 6.اغلاق الهاتف عن طريق الايبي] ")
+a= input("اختر اداة من القائمة السابقة: ")
+if a =="1":
+	webbrowser.open("https://grabify.link/")
+elif a=="2":
+	webbrowser.open("https://stopncii.org/")
+elif a=="3":
+	webbrowser.open("https://moi.gov.eg/")
+elif a=="4":
+	webbrowser.open("https://youtu.be/tp5vdd2hquk")
+elif a=="5":
+	webbrowser.open("https://play.google.com/store/apps/details?id=com.jotform.v2")
+elif a=="6":
+	webbrowser.open("https://youtu.be/WMfaxsiAOGs")
